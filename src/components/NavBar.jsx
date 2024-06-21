15db34a6c38bd94ef67be076af327a3efc9f2c2a
annCMasana <annmasana@example.com>
2024-06-22 00:37:42 +0800

    Added Home Page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import './NavBar.css';
-import devDateLogo1 from '../assets/devDateLogo1.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import "./NavBar.css";
+import devDateLogo1 from "../assets/devDateLogo1.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   return (
@@ -11,21 +11,43 @@ const NavBar = () => {
       <nav>
         <input type="checkbox" id="check" />
         <label htmlFor="check" className="checkbtn">
-          <box-icon name='menu' color='white'></box-icon>
+          <box-icon name="menu" color="white"></box-icon>
         </label>
         <div className="navbar-logo">
-          <img src={devDateLogo1} alt="Logo" />
+          <Link to="homePage">
+            <img src={devDateLogo1} alt="Logo" />
+          </Link>
         </div>
         <div className="navbar-search">
           <div className="search-container">
             <FontAwesomeIcon icon={faSearch} className="search-icon" />
-            <input type="text" placeholder="Search..." className="search-input" />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="search-input"
+            />
           </div>
         </div>
         <ul>
-          <li><Link to='homePage'><box-icon name='heart' color='white' size='28px'></box-icon></Link></li>
-          <li><Link to='messagesPage'><box-icon name='message-rounded-dots' color='white' size='28px'></box-icon></Link></li>
-          <li><Link to='settingPage'><box-icon name='cog' color='white' size='28px'></box-icon></Link></li>
+          <li>
+            <Link to="homePage">
+              <box-icon name="heart" color="white" size="28px"></box-icon>
+            </Link>
+          </li>
+          <li>
+            <Link to="messagesPage">
+              <box-icon
+                name="message-rounded-dots"
+                color="white"
+                size="28px"
+              ></box-icon>
+            </Link>
+          </li>
+          <li>
+            <Link to="settingPage">
+              <box-icon name="cog" color="white" size="28px"></box-icon>
+            </Link>
+          </li>
         </ul>
       </nav>
       <Outlet />
--- src/pages/user/HomePage/HomePage.jsx
+++ src/pages/user/HomePage/HomePage.jsx
@@ -1,9 +1,116 @@
-import React from 'react'
+import React from "react";
+import "./HomePage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faEye } from "@fortawesome/free-solid-svg-icons";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
+
+import slide_img1 from "../../../assets/profiles/profile_1.jfif";
+import slide_img2 from "../../../assets/profiles/profile_2.jfif";
+import slide_img3 from "../../../assets/profiles/profile_3.jfif";
+import slide_img4 from "../../../assets/profiles/profile_4.jfif";
+import slide_img5 from "../../../assets/profiles/profile_5.jfif";
+import user from "../../../assets/user.png";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="homePage-main-container">
+      <div className="container-fluid">
+        <div className="header">Try and Catch</div>
+        <div className="main-content">
+          <div className="left-pane">
+            <Swiper
+              effect={"coverflow"}
+              grabCursor={true}
+              centeredSlides={true}
+              loop={true}
+              slidesPerView={"auto"}
+              coverflowEffect={{
+                rotate: 0,
+                stretch: 0,
+                depth: 100,
+                modifier: 2.5,
+              }}
+              pagination={{ el: ".swiper-pagination", clickable: true }}
+              navigation={{
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+                clickable: true,
+              }}
+              modules={[EffectCoverflow, Pagination, Navigation]}
+              className="swiper_container"
+            >
+              <SwiperSlide>
+                <img src={slide_img1} alt="slide_image" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={slide_img2} alt="slide_image" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={slide_img3} alt="slide_image" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={slide_img4} alt="slide_image" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={slide_img5} alt="slide_image" />
+              </SwiperSlide>
+              {/* <div className="slider-controler">
+                <div className="swiper-button-prev slider-arrow">
+                  <FontAwesomeIcon icon={faAngleLeft} className="arrow-icon" />
+                </div>
+                <div className="swiper-button-next slider-arrow">
+                  <FontAwesomeIcon icon={faAngleRight} className="arrow-icon" />
+                </div>
+                <div className="swiper-pagination"></div>
+              </div> */}
+            </Swiper>
+            <div className="footer">
+              <div className="icon-buttons">
+                <div>
+                  <FontAwesomeIcon icon={faEye} className="icon-btn" />
+                </div>
+                <div>
+                  <FontAwesomeIcon icon={faHeart} className="icon-btn" />
+                </div>
+              </div>
+            </div>
+            <div className="extra-footer">Find Friends to Try and Catch</div>
+          </div>
+          <div className="right-pane">
+            <div className="right-pane-header">
+              <span>Suggested for you</span>
+              <span>See All</span>
+            </div>
+            <div className="suggestions">
+              {[...Array(5)].map((_, index) => (
+                <div className="suggestion-item" key={index}>
+                  <img src={user} alt="Suggestion" />
+                  <div>
+                    <p>Tristan Angelo</p>
+                    <p>Follows you</p>
+                  </div>
+                  <button
+                    className={`follow-btn ${
+                      index % 2 === 0 ? "follow" : "followed"
+                    }`}
+                  >
+                    {index % 2 === 0 ? "Follow" : "Followed"}
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
